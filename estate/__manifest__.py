9352b6256c247f896be2c19af97cbed484d3faab
Weblife123 <rashid1993anwer@gmail.com>
2025-08-24 17:02:09 +0400

    Update __manifest__.py

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Rashid Anwer",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    
+    # data files containing optionally loaded demonstration data
+   
+}
